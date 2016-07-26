b09e8094190260f629f819066bc722c5319b98ac
KSureshB <sureshbabuitsoft@gmail.com>
2016-07-26 12:52:29 +0530

    palindrom

---
+++ lib/palin.rb
@@ -0,0 +1,26 @@
+require "palin/version"
+
+module Palin
+  # Your code goes here...
+  	class Palindrom
+		def reverse(word_arr)
+  			reverse = []
+  			index = word_arr.length
+  			until index == 0 do
+   				 reverse << word_arr[index - 1]
+    				index -= 1
+ 			 end
+ 		 reverse
+		end
+
+		def is_palindrome?(word)
+		  word_arr = word.downcase.gsub(/ /,'').split('')
+  			if word_arr == reverse(word_arr)
+				return "entered string is palindrome"
+			else
+				return "entered string is not a palindrome"
+			end
+  
+		end
+	end
+end
---
+++ lib/palin/version.rb
@@ -0,0 +1,3 @@
+module Palin
+  VERSION = "0.1.0"
+end
